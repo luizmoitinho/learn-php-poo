4bf035a02b6c6e4d2b44c800abe82e2761bdd9dd
luizmoitinho <luizcarlos_costam@hotmail.com>
2019-07-03 14:50:33 -0300

    Primeiros Exemplos

--- arrays.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $meu_array =  array("luiz", "carlos","joao");
-
-    foreach($meu_array as $pessoa)
-        echo "$pessoa<br>";
-?>
--- variaveis.php
+++
@@ -1,9 +0,0 @@
-<?php
-    echo "Manipulaçao de Arrayss";
-    $mensagem[4];
-
-    echo"<br><Br>";
-    for ($i=0;$i<4;$i++){
-        echo "Valor :$i =  $mensagem[$i]<br>";
-    }
-?>
